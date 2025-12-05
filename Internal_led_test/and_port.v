6c55ba02443e2aebb08b72db3c6ed2dc4c16cd15
Mirko Genoni <mirko.genoni@mail.polimi.it>
2025-12-05 01:32:06 +0100

    remove useless comments

--- Internal_led_test/and_port.v
+++ Internal_led_test/and_port.v
@@ -1,4 +1,4 @@
-module top(						//top module
+module top(
 	CLK,
 	BUT1,
 	BUT2,
@@ -6,14 +6,14 @@ module top(						//top module
 	LED2
 );
 
-input			CLK;				//input 100Mhz clock
-input			BUT1;				//input signal from button 1
-input			BUT2;				//input signal from button 2
-output			LED1;				//output signal to LED1
-output			LED2;				//output signal to LED2
+input			CLK;
+input			BUT1;
+input			BUT2;
+output			LED1;
+output			LED2;
 
-reg			BUT1_r=1'b0;				//register to keep button 1 state
-reg			BUT2_r=1'b0;				//register to keep button 2 state
+reg			BUT1_r=1'b0;
+reg			BUT2_r=1'b0;
 
 reg			LED_state=1'b0;
 reg			[14:0] rst_count=0;
@@ -21,12 +21,12 @@ reg			[14:0] rst_count=0;
 assign LED1 = LED_state;
 assign LED2 = LED_state;
 
-always @ (posedge CLK) begin			//on each positive edge of 24414Hz clock
-	BUT1_r <= BUT1;					//capture button 1 state to BUT1_r
-	BUT2_r <= BUT2;					//capture button 2 state to BUT2_r
+always @ (posedge CLK) begin
+	BUT1_r <= BUT1;
+	BUT2_r <= BUT2;
 		
 	if(BUT1_r == 1'b0 && BUT2_r == 1'b0) begin
-		LED_state <= 1'b1;				//copy inversed state of button 1 to LED1_m0_r
+		LED_state <= 1'b1;
 	end else begin
 		LED_state <= 1'b0;
 	end
